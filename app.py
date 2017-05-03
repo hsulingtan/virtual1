66fe62fff1db143706f2953baef953dfae850af5
Ellen Tan <garnet.mermaid@gmail.com>
2017-05-03 13:52:17 +0800

    flask first

---
+++ app.py
@@ -0,0 +1,17 @@
+#Flask hello world#
+from flask import Flask
+
+#create application object
+app = Flask(__name__)
+
+#use decorator pattern to link the view function to a url
+@app.route("/")
+@app.route("/hello")
+
+#define the view using a function, which returns a string
+def hello_world():
+    return "Hello, World!"
+
+#start the development server using the run() method
+if __name__ == "__main__":
+	app.run()
